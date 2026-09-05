41f7cda5b9b75434b8eea5ccde3915131cf4642a
Mohammed Syed <77437373+mohammeds123@users.noreply.github.com>
2026-09-05 15:50:50 -0600

    Add: two-integer-sum - submission-6

---
+++ Data Structures & Algorithms/two-integer-sum/submission-6.py
@@ -0,0 +1,17 @@
+class Solution:
+    def twoSum(self, nums: List[int], target: int) -> List[int]:
+        hashtable = {}
+        result = [0, 0]
+        count = 0
+        for i in range(len(nums)): 
+            hashtable[nums[i]] = count
+            count += 1
+        for j in range(len(nums)):
+            diff = target - nums[j]
+            if diff in hashtable and hashtable[diff] != j:
+                result[0] = j
+                result[1] = hashtable.get(diff)
+                break
+        return result
+
+        
